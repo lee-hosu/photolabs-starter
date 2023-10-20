55d8689f929bbe2d0a0fcec3d4ad5162c64a7f2c
Hosu Lee <sogeumhosu@gmail.com>
2023-10-19 20:55:29 -0700

    Create top navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-// import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
+import TopNavigationBar from './components/TopNavigationBar';
 
 import './App.scss';
 
-
-
-// const photos = new Array(3).fill(sampleDataForPhotoListItem); //Create an array with a length of 3
-
-// Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigationBar/>
       <PhotoList/>
     </div>
   );
